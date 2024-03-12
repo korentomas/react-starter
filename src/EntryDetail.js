627586ba9455ff7ab32475bc52c1cb7e281d3cf5
koren <tomaskorenblit@gmail.com>
2024-03-12 18:06:16 -0300

    oops, edited wrong image.

--- src/EntryDetail.js
+++ src/EntryDetail.js
@@ -3,7 +3,7 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 const EntryDetail = ({ image, alt, name, content }) => {
   return (
     <div className="entry-detail">
-      <img src={image} alt={alt} className="entry-image" />
+      {/* <img src={image} alt={alt} className="entry-image" /> */}
       <h2 className="entry-title">{name}</h2>
       <div className="entry-content">
         {documentToReactComponents(content)}
